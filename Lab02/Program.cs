5acddc2e1ae53688a44ef9b162b500ee5def5032
bikezclxq1 <doshick1907@gmail.com>
2026-08-25 11:05:47 +0700

    Add lab02 Part A
    ทำไปแค่ A

--- Lab02/Program.cs
+++ Lab02/Program.cs
@@ -12,7 +12,43 @@ namespace Lab02
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string bossName = "Kirin";
+            char rank = 'S';
+            int level = 7;
+            int maxHp = 240;
+            int currentHp = 175;
+            float attackPower = 42.5f;
+            double critMultiplier = 1.75;
+            bool isBoss = true;
+
+            Console.WriteLine("===== BOSS STATUS: INITIAL =====");
+            Console.WriteLine($"Name: {bossName}");
+            Console.WriteLine($"Rank: {rank}");
+            Console.WriteLine($"HP: {currentHp} / {maxHp}");
+            Console.WriteLine($"Attack power: {attackPower}");
+            Console.WriteLine($"Crit Multiplier: {critMultiplier}");
+            Console.WriteLine($"Is boss: {isBoss}");
+           
+
+            Console.WriteLine();
+            // คำนวนเปอร์เซ็น
+            int hpPercent = currentHp * 100 / maxHp;
+            Console.WriteLine($"HP Percent: {hpPercent}") ;
+            Console.WriteLine();
+            Console.WriteLine("Kirin takes 60 damages!");
+            Console.WriteLine() ;
+            // ลด HP 60 หน่วย
+            currentHp = currentHp - 60;
+            Console.WriteLine();
+            Console.WriteLine("==== BOSS STATUS: AFTER DAMAGE ====");
+            Console.WriteLine($"HP: {currentHp} / {maxHp}");
+            hpPercent = currentHp * 100 / maxHp;
+            Console.WriteLine($"HP Percent: {hpPercent}%");
+
+
+
+
+
         }
     }
 }
