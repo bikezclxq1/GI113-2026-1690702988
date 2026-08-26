f6e82150336125f1d4e8a508a2b1b24a8cabf746
bikezclxq1 <doshick1907@gmail.com>
2026-08-26 14:11:01 +0700

    Lab02 Sedlaewkubpom
    Sedlaewkubpom

--- Lab02/Program.cs
+++ Lab02/Program.cs
@@ -47,6 +47,70 @@ namespace Lab02
 
 
 
+            // Part B
+            Console.WriteLine();
+            Console.WriteLine(" ## PLAYER 1 : EREN ## ");
+            string player1 = "EREN";
+            char rank1 = 'S';
+            int maxHp1 = 100;
+            float damage1 = 25.00f;
+            double speed1 = 20;
+            bool isHandsome1 = true;
+            Console.WriteLine($"Name: {player1}");
+            Console.WriteLine($"Rank: {rank1}");
+            Console.WriteLine($"Hp: {maxHp1}");
+            Console.WriteLine($"Damage: {damage1}");
+            Console.WriteLine($"Speed : {speed1}");
+            Console.WriteLine($"Handsome: {isHandsome1}");
+
+            Console.WriteLine(" ## PLAYER 2 : LEVI ## ");
+            string player2 = "Levi";
+            char rank2 = 'S';
+            int maxHp2 = 100;
+            float damage2 = 42.00f;
+            double speed2 = 42;
+            bool isHandsome2 = true;
+            Console.WriteLine($"Name: {player2}");
+            Console.WriteLine($"Rank: {rank2}");
+            Console.WriteLine($"Hp: {maxHp2}");
+            Console.WriteLine($"Damage: {damage2}");
+            Console.WriteLine($"Speed : {speed2}");
+            Console.WriteLine($"Handsome: {isHandsome2}");
+
+            Console.WriteLine(" ## PLAYER 3 : ARMIN ## ");
+            string player3 = "Armin";
+            char rank3 = 'S';
+            int maxHp3 = 100;
+            float damage3 = 30.00f;
+            double speed3 = 30;
+            bool isHandsome3 = false;
+            Console.WriteLine($"Name: {player2}");
+            Console.WriteLine($"Rank: {rank2}");
+            Console.WriteLine($"Hp: {maxHp2}");
+            Console.WriteLine($"Damage: {damage2}");
+            Console.WriteLine($"Speed : {speed2}");
+            Console.WriteLine($"Handsome: {isHandsome3}");
+
+            Console.WriteLine(" ## PLAYER 4 : MIKASA ## ");
+            string player4 = "Mikasa";
+            char rank4 = 'S';
+            int maxHp4 = 100;
+            float damage4 = 40.00f;
+            double speed4 = 40;
+            bool isHandsome4 = false;
+            Console.WriteLine($"Name: {player4}");
+            Console.WriteLine($"Rank: {rank4}");
+            Console.WriteLine($"Hp: {maxHp4}");
+            Console.WriteLine($"Damage: {damage4}");
+            Console.WriteLine($"Speed : {speed4}");
+            Console.WriteLine($"Handsome: {isHandsome4}");
+
+
+
+
+
+
+
 
 
         }
